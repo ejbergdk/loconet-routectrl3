fca36dbfec8b7c60c0491573a7dcfe72b729d1c1
Mikael Ejberg Pedersen <83463793+ejbergdk@users.noreply.github.com>
2023-12-31 15:00:50 +0100

    Update base code and submodules to latest

--- routectrl3/timer.h
+++ routectrl3/timer.h
@@ -9,12 +9,13 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
+#include <stdint.h>
 #include "ticks.h"
 
 
 typedef void    (timer_cb) (void *);
 
-extern void     timer_add(ticks_t timeout, timer_cb *cb, void *ctx);
+extern int8_t   timer_add(ticks_t timeout, timer_cb *cb, void *ctx);
 extern void     timer_update(void);
 
 #endif /* TIMER_H_ */
